5322b4faf5d6b357865a1a91f7be7d1d6ed7352b
Vikas Ukani <vikasukani5@gmail.com>
2022-09-25 12:18:00 +0530

    Done: Tailwind and JS Configuration complete.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import DefaultLayout from 'Components/Layout/DefaultLayout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
+  )
+
 }
 
 export default MyApp
